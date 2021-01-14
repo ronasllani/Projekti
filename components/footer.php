d59fa08f9c00acee32d0190b227ea17f5470feab
Enes Shabani <es48843@ubt-uni.net>
2021-01-14 23:18:44 +0100

    Konvertimi prej html ne php (header footer)

---
+++ components/footer.php
@@ -0,0 +1,25 @@
+<div class="footer-div">
+<div class="shija-footer">
+    <h3 class="shija-titulli">Shija</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing<br> elit. Dolorum ipsum iure libero necessitatibus porro, <br>quibusdam ratione animi </p>
+</div>
+<div class="footer-nav">
+    <h3 class="nav-titulli">Pages</h3>
+    <div class="nav-elements">
+        <a href="home.php"><p >Home</p></a>
+        <a href="menu.php"><p>Menu</p></a>
+        <a href="contact-us.php"><p>Contact Us</p></a>
+        <a href="login.php"><p>Login / Register</p></a>
+    </div>
+</div>
+<div class="footer-contact">
+    <h3 class="nav-titulli">Contact</h3>
+    <p>Kosove, Prishtine</p>
+    <p>Lagja NR, Str, 10000</p>
+    <div class="fcontact-a">
+        <a href="tel:5554280940">+ 123 4567 89</a>
+        <a href="mailto:info@yoursite.com">info@yoursite.com</a>
+    </div>
+</div>
+</div>
+<h5 class="footer-content">Shija Restaurant &#169; All rights reserved</h5>
--- contact-us.php
+++ contact-us.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <?php include ('activenav.php');?>
         <title>Contact Us</title>
         <link rel="stylesheet" href="css/style.css">
         <link rel="shortcut icon" href="img/logo.ico" type="image/x-icon">
@@ -9,15 +10,7 @@
 
     <body>
         <header>
-            <a href="home.php" class="logo"><img class="logo" src="img/logo.png" ></a>
-            <nav>
-                <ul class="nav-links">
-                    <li><a href="home.php" >Home</a></li>
-                    <li><a href="menu.php" >Menu</a></li>
-                    <li><a href="contact-us.php" class="active">Contact Us</a></li>
-                </ul>
-            </nav>            
-            <a class="lgr" href="login.php"><button class="navbut">Log In / Register</button></a>
+         <?php include ('./components/header.php');?>    
         </header>
         <main>
             <div class="contacttext">
@@ -51,32 +44,7 @@
 
         </main>
         <footer>
-            <div class="footer-div">
-                <div class="shija-footer">
-                    <h3 class="shija-titulli">Shija</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing<br> elit. Dolorum ipsum iure libero necessitatibus porro, <br>quibusdam ratione animi </p>
-                </div>
-                <div class="footer-nav">
-                    <h3 class="nav-titulli">Pages</h3>
-                    <div class="nav-elements">
-                        <a href="home.php"><p >Home</p></a>
-                        <a href="menu.php"><p>Menu</p></a>
-                        <a href="contact-us.php"><p>Contact Us</p></a>
-                        <a href="login.php"><p>Login / Register</p></a>
-                    </div>
-                </div>
-                <div class="footer-contact">
-                    <h3 class="nav-titulli">Contact</h3>
-                    <p>Kosove, Prishtine</p>
-                    <p>Lagja NR, Str, 10000</p>
-                    <div class="fcontact-a">
-                        <a href="tel:5554280940">+ 123 4567 89</a>
-                        <a href="mailto:info@yoursite.com">info@yoursite.com</a>
-                    </div>
-                </div>
-            </div>
-            <h5 class="footer-content">Shija Restaurant &#169; All rights reserved</h5>
-
+            <?php include ('./components/footer.php');?>    
         </footer>
         <script src="javascript/contact-us.js"></script>
     </body>
