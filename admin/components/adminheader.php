6bae74c6e8e9f076a8d2c0b1cff604d482b815f9
Enes Shabani <es48843@ubt-uni.net>
2021-02-14 18:56:34 +0100

    ndryshimi i menus nga admin dashboard

--- admin/components/adminheader.php
+++ admin/components/adminheader.php
@@ -5,6 +5,7 @@
         <li><a href="create-user.php" class="<?php active('create-user.php');?>">Krijo User</a></li>
         <li><a href="mesazhet.php" class="<?php active('mesazhet.php');?>">Mesazhet</a></li>
         <li><a href="users.php" class="<?php active('users.php');?>">Users</a></li>
+        <li><a href="adminmenu.php" class="<?php active('adminmenu.php');?>">Menu</a></li>
     </ul>
 </nav>            
 <a class="lgr" href="admin.php?logout='1'"><button class="navbut">Logout</button></a>
